898011288fd2302bc5784eb524600da8d4f6d177
lenarts <lenart.strgar@gmail.com>
2025-10-23 17:15:36 +0200

    Delete web.js

--- web.js
+++
@@ -1,6 +0,0 @@
-import http from "http";
-const port = process.env.PORT || 8080;
-http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Minecraft server running!");
-}).listen(port, () => console.log("Ping server active on port " + port));
