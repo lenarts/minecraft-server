bdff427e9f7d7e9cca75bb4206f4984e25abfd2d
lenarts <lenart.strgar@gmail.com>
2025-10-23 19:42:08 +0200

    Update web.js

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ import http from "http";
 const port = process.env.PORT || 8080;
 http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Minecraft server running!");
+  res.end("Hi!");
 
 }).listen(port, () => console.log("Ping server active on port " + 25565));
+
